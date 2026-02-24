7593e683d215bc5d891315260308bf93575d76c4
Stacey Son <sson@me.com>
2026-02-24 07:50:26 -0600

    Update start_local_sd.sh to use gsgparm

--- start_local_sd.sh
+++ start_local_sd.sh
@@ -6,6 +6,9 @@ export HOME=/sdcard
 export XDG_CONFIG_HOME=/sdcard
 export XDG_RUNTIME_DIR=/sdcard
 
+# Add /sdcard/bin to the path.
+export PATH="/sdcard/bin:$PATH"
+
 GAME_PATH=
 GAME_LIB=
 
@@ -14,202 +17,120 @@ GAME_LIB=
 case "$4" in
   /sdcard/Games/Atari\ 2600/*)
     GAME_LIB=stella2014_libretro.so
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led18/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 B,X,Y
     ;;
   /sdcard/Games/Atari\ 2600\ Paddle/*)
     GAME_LIB=stella
-    echo 255 > /sys/class/leds/key_led14/brightness
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led18/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 B,X,Y -1 spin
     ;;
   /sdcard/Games/Atari\ 5200/*)
     #GAME_LIB=atari800_libretro.so
     GAME_LIB=a5200_libretro.so
     #Turn on keypad
-    echo 21 > /sys/devices/platform/usb_control/key_mode
-    echo 255 > /sys/class/leds/key_led1/brightness
-    echo 255 > /sys/class/leds/key_led2/brightness
-    echo 255 > /sys/class/leds/key_led3/brightness
-    echo 255 > /sys/class/leds/key_led4/brightness
-    echo 255 > /sys/class/leds/key_led5/brightness
-    echo 255 > /sys/class/leds/key_led6/brightness
-    echo 255 > /sys/class/leds/key_led7/brightness
-    echo 255 > /sys/class/leds/key_led8/brightness
-    echo 255 > /sys/class/leds/key_led9/brightness
-    echo 255 > /sys/class/leds/key_led10/brightness
-    echo 255 > /sys/class/leds/key_led11/brightness
-    echo 255 > /sys/class/leds/key_led12/brightness
-    echo 0 > /sys/class/leds/key_led18/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 X,Y -1 1,2,3,4,5,6,7,8,9,0,S,P -k 21
     ;;
   /sdcard/Games/Atari\ 7800/*)
     GAME_LIB=prosystem_nds_libretro.so
-    echo 0 > /sys/class/leds/key_led18/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 X,Y
     ;;
   /sdcard/Games/Atari\ Lynx/*)
     GAME_LIB=handy_libretro.so
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 B,Y
     ;;
   /sdcard/Games/ColecoVision/*)
     GAME_LIB=gearcoleco_libretro.so
-    echo 21 > /sys/devices/platform/usb_control/key_mode
-    echo 255 > /sys/class/leds/key_led1/brightness
-    echo 255 > /sys/class/leds/key_led2/brightness
-    echo 255 > /sys/class/leds/key_led3/brightness
-    echo 255 > /sys/class/leds/key_led4/brightness
-    echo 255 > /sys/class/leds/key_led5/brightness
-    echo 255 > /sys/class/leds/key_led6/brightness
-    echo 255 > /sys/class/leds/key_led7/brightness
-    echo 255 > /sys/class/leds/key_led8/brightness
-    echo 255 > /sys/class/leds/key_led9/brightness
-    echo 255 > /sys/class/leds/key_led10/brightness
-    echo 255 > /sys/class/leds/key_led11/brightness
-    echo 255 > /sys/class/leds/key_led12/brightness
-    ;;  
+    gsgparm -0 X,Y -1 1,2,3,4,5,6,7,8,9,0,S,P -k 21
+    ;;
   /sdcard/Games/Doom/*)
     GAME_LIB=prboom_libretro.so
-    echo 21 > /sys/devices/platform/usb_control/key_mode
-    echo 255 > /sys/class/leds/key_led5/brightness
-    echo 255 > /sys/class/leds/key_led20/brightness
-    echo 255 > /sys/class/leds/key_led21/brightness
-    echo 255 > /sys/class/leds/key_led22/brightness
-    echo 255 > /sys/class/leds/key_led23/brightness
+    gsgparm -0 X,Y -1 5,L1,L2,R1,R2 -k 21
     ;;
   /sdcard/Games/Dreamcast/*)
     GAME_LIB=flycast_libretro.so
     ;;
   /sdcard/Games/Gameboy/*)
     GAME_LIB=gambatte_libretro.so
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
-    ;;  
+    gsgparm -0 B,Y
+    ;;
   /sdcard/Games/Gameboy\ Advance/*)
     GAME_LIB=mgba_libretro.so
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
-    echo 255 > /sys/class/leds/key_led20/brightness
-    echo 255 > /sys/class/leds/key_led22/brightness
+    gsgparm -0 B,Y -1 L1,R1
     ;;
   /sdcard/Games/Gameboy\ Color/*)
     GAME_LIB=gambatte_libretro.so
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
-    ;;  
+    gsgparm -0 B,Y
+    ;;
   /sdcard/Games/Intellivision/*)
     GAME_LIB=freeintv_libretro.so
-    echo 21 > /sys/devices/platform/usb_control/key_mode
-    echo 255 > /sys/class/leds/key_led1/brightness
-    echo 255 > /sys/class/leds/key_led2/brightness
-    echo 255 > /sys/class/leds/key_led3/brightness
-    echo 255 > /sys/class/leds/key_led4/brightness
-    echo 255 > /sys/class/leds/key_led5/brightness
-    echo 255 > /sys/class/leds/key_led6/brightness
-    echo 255 > /sys/class/leds/key_led7/brightness
-    echo 255 > /sys/class/leds/key_led8/brightness
-    echo 255 > /sys/class/leds/key_led9/brightness
-    echo 255 > /sys/class/leds/key_led10/brightness
-    echo 255 > /sys/class/leds/key_led11/brightness
-    echo 255 > /sys/class/leds/key_led12/brightness
-    ;;  
+    gsgparm -0 X,Y -1 1,2,3,4,5,6,7,8,9,0,S,P -k 21
+    ;;
   /sdcard/Games/Mame/*)
     GAME_LIB=mame2003_libretro.so
     #Turn on trackball and spinner
-    echo 255 > /sys/class/leds/key_led13/brightness
-    echo 255 > /sys/class/leds/key_led14/brightness
-    echo 22 > /sys/devices/platform/usb_control/key_mode      
+    gsgparm -1 tball,spin -k 22
     ;;
   /sdcard/Games/Mame\ 2003\ Plus/*)
     GAME_LIB=mame2003_plus_libretro.so
-    echo 255 > /sys/class/leds/key_led13/brightness
-    echo 255 > /sys/class/leds/key_led14/brightness
-    echo 22 > /sys/devices/platform/usb_control/key_mode      
+    gsgparm -1 tball,spin -k 22
     ;;
   /sdcard/Games/Neo\ Geo/*)
     GAME_LIB=fbneo_libretro.so
      ;;
   /sdcard/Games/Neo\ Geo\ Pocket\ Color/*)
     GAME_LIB=mednafen_ngp_libretro.so
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 B,Y
     ;;
   /sdcard/Games/NES/*)
     GAME_LIB=fceumm_libretro.so
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 B,Y
     ;;
   /sdcard/Games/PC\ Engine/*)
     GAME_LIB=mednafen_pce_fast_libretro.so
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 B,Y
     ;;
   /sdcard/Games/pcenginecd/*)
     GAME_LIB=mednafen_pce_fast_libretro.so
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 B,Y
     ;;
   /sdcard/Games/PlayStation/*)
     GAME_LIB=pcsx_rearmed_libretro.so
-    echo 255 > /sys/class/leds/key_led7/brightness
-    echo 255 > /sys/class/leds/key_led8/brightness
-    echo 255 > /sys/class/leds/key_led9/brightness
-    echo 255 > /sys/class/leds/key_led20/brightness
-    echo 255 > /sys/class/leds/key_led21/brightness
-    echo 255 > /sys/class/leds/key_led22/brightness
-    echo 255 > /sys/class/leds/key_led23/brightness
+    gsgparm -1 7,8,9,L1,L2,R1,R2
     ;;
   /sdcard/Games/ScummVM/*)
     GAME_LIB=scummvm_libretro.so
     #Give ScummVM the trackball for the mouse pointer
-    echo 255 > /sys/class/leds/key_led5/brightness
-    echo 255 > /sys/class/leds/key_led11/brightness
-    echo 255 > /sys/class/leds/key_led13/brightness
-    echo 255 > /sys/class/leds/key_led20/brightness
-    echo 255 > /sys/class/leds/key_led22/brightness
-    echo 255 > /sys/class/leds/key_led23/brightness
+    gsgparm -1 5,0,tball,L1,R1,R2
     ;;
   /sdcard/Games/Sega\ 32x/*)
     GAME_LIB=picodrive_libretro.so
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 Y
     ;;
   /sdcard/Games/Sega\ CD/*)
     GAME_LIB=genesis_plus_gx_libretro.so
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 Y
     ;;
   /sdcard/Games/Sega\ Game\ Gear/*)
     GAME_LIB=picodrive_libretro.so
-    echo 0 > /sys/class/leds/key_led18/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 X,Y
     ;;
   /sdcard/Games/Sega\ Genesis/*)
     GAME_LIB=picodrive_libretro.so
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 Y
     ;;
   /sdcard/Games/Sega\ Master\ System/*)
     GAME_LIB=picodrive_libretro.so
-    echo 0 > /sys/class/leds/key_led18/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
+    gsgparm -0 X,Y
     ;;
   /sdcard/Games/SNES/*)
     GAME_LIB=snes9x2005_plus_libretro.so
-    echo 255 > /sys/class/leds/key_led20/brightness
-    echo 255 > /sys/class/leds/key_led22/brightness
+    gsgparm -1 L1,R1
     ;;
   /sdcard/Games/Virtual\ Boy/*)
     GAME_LIB=mednafen_vb_libretro.so
     ;;
   /sdcard/WonderSwan\ Color/*)
     GAME_LIB=mednafen_wswan_libretro.so
-    echo 0 > /sys/class/leds/key_led17/brightness
-    echo 0 > /sys/class/leds/key_led19/brightness
-    echo 255 > /sys/class/leds/key_led20/brightness
-    echo 255 > /sys/class/leds/key_led21/brightness
-    echo 255 > /sys/class/leds/key_led22/brightness
-    echo 255 > /sys/class/leds/key_led23/brightness
+    gsgparm -0 B,Y -1 L1,R1,L2,R2
     ;;
   *)
     GAME_LIB=NONE
@@ -248,10 +169,7 @@ esac
       #Turn on the keypad for hot keys (1 to save state, 3 to load state, 5 for screenshots).
       #Save states are set by the cfg, they should be in /sdcard/retroarch/states/<emulator core name>
       #Screenshots are set by the cfg, they should be in /sdcard/retroarch/screenshots/<emulator core name>
-      echo 18 > /sys/devices/platform/usb_control/key_mode
-      echo 255 > /sys/class/leds/key_led1/brightness
-      echo 255 > /sys/class/leds/key_led3/brightness
-      echo 255 > /sys/class/leds/key_led5/brightness
+      gsgparm -1 1,3,5 -k 18
       #Run any other game
       /tmp/retroarch\
         -c ~/retroarch/config/retroarch.cfg\
@@ -260,29 +178,7 @@ esac
   esac
 
   #Exiting game, resetting button LEDs
-  echo 0 > /sys/class/leds/key_led1/brightness
-  echo 0 > /sys/class/leds/key_led2/brightness
-  echo 0 > /sys/class/leds/key_led3/brightness
-  echo 0 > /sys/class/leds/key_led4/brightness
-  echo 0 > /sys/class/leds/key_led5/brightness
-  echo 0 > /sys/class/leds/key_led6/brightness
-  echo 0 > /sys/class/leds/key_led7/brightness
-  echo 0 > /sys/class/leds/key_led8/brightness
-  echo 0 > /sys/class/leds/key_led9/brightness
-  echo 0 > /sys/class/leds/key_led10/brightness
-  echo 0 > /sys/class/leds/key_led11/brightness
-  echo 0 > /sys/class/leds/key_led12/brightness
-  echo 0 > /sys/class/leds/key_led13/brightness
-  echo 0 > /sys/class/leds/key_led14/brightness
-  echo 255 > /sys/class/leds/key_led15/brightness
-  echo 255 > /sys/class/leds/key_led16/brightness
-  echo 255 > /sys/class/leds/key_led17/brightness
-  echo 255 > /sys/class/leds/key_led18/brightness
-  echo 255 > /sys/class/leds/key_led19/brightness
-  echo 0 > /sys/class/leds/key_led20/brightness
-  echo 0 > /sys/class/leds/key_led21/brightness
-  echo 0 > /sys/class/leds/key_led22/brightness
-  echo 0 > /sys/class/leds/key_led23/brightness
+  gsgparm -0 1,2,3,4,5,6,7,8,9,0,S,P,tball,spin,L1,L2,R1,R2 -1 dpad,A,B,X,Y
 
 #checking to see if the user is trying to reset to the main menu
 evtest --query /dev/input/event1 1 16
